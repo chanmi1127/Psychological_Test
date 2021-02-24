e8b460c9d07ea0ea02c0a619c54bf8e95a7fc310
Chanmi <chanmi1127@gmail.com>
2021-02-24 22:58:09 +0900

    CL 20210224 task revised

--- my-app/src/Test.js
+++ my-app/src/Test.js
@@ -1,45 +1,109 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useMemo, useCallback} from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
 
 function Test() {
-    const [data, setData] = useState([]);
+    const [questions, setQuestions] = useState([]);
+    const [answers, setAnswers] = useState([]);
+    const [page, setPage] = useState(0);
 
-    async function fetchTest() {
-        var apiUrl  = 'https://www.career.go.kr/inspct/openapi/test/questions?apikey=8b75f809812e0d513b4789912f3513cd&q=6';
+    var apiUrl  = 'https://www.career.go.kr/inspct/openapi/test/questions?apikey=8b75f809812e0d513b4789912f3513cd&q=6';
+
+    const fetchQuestions = useCallback(async  () => {
         const response = await axios.get(apiUrl);
-        console.log(response.data.RESULT);
-        setData(response.data.RESULT);
-    }
+        setQuestions(response.data.RESULT);
+        setAnswers(() => {
+            return new Array(response.data.RESULT.length);
+        });
+    },[apiUrl]);
+    
+    const visibleQuestions = useMemo(() => {
+        return questions.slice(page*5, (page+1)*5);
+    }, [page, questions]);
+
+    const isButtonDisabled = useMemo(() => {
+        let isDisabled = false;
+        visibleQuestions.forEach((question) => {
+          const index = parseInt(question.qitemNo, 10);
+          if (!answers[index - 1]) {
+            isDisabled = true;
+          }
+        });
+        return isDisabled;
+      }, [answers, visibleQuestions]);
+
 
     useEffect(() => {
-        fetchTest();
-    }, []);
-
-    const questions = data.map((data) => (
-        <li key={data.qitemNo}>
-            {data.question} <br/>
-            <input type="radio" name={data.qitemNo} value={data.answerScore01} />
-            {data.answer01} 
-            &nbsp;
-            <input type="radio" name={data.qitemNo} value={data.answerScore02} />
-            {data.answer02}
-        </li>)
-    );
+        fetchQuestions();
+    }, [fetchQuestions]);
 
     return (
         <div>
-            <h2>검사 진행</h2>
-            <div>
-                {questions}
-            </div>
-            <button>이전</button>
-            <button>다음</button>
-            <Link to='/testfinished'>
-                <button>제출</button>
-            </Link> 
+          <div>
+            {visibleQuestions.map((question) => {
+              const qitemNo = parseInt(question.qitemNo, 10);
+    
+              return (
+                <div key={qitemNo}>
+                  <h3>{question.question}</h3>
+                  <div>
+                    <label>
+                      <input
+                        type="radio"
+                        name={`answers[${qitemNo - 1}]`}
+                        onChange={() => {
+                          setAnswers((current) => {
+                            const newAnswers = [...current];
+                            newAnswers[qitemNo - 1] = question.answerScore01;
+                            return newAnswers;
+                          });
+                        }}
+                      />
+                      {question.answer01}
+                    </label>
+    
+                    <label>
+                      <input
+                        type="radio"
+                        name={`answers[${qitemNo - 1}]`}
+                        onChange={() => {
+                          setAnswers((current) => {
+                            const newAnswers = [...current];
+                            newAnswers[qitemNo - 1] = question.answerScore02;
+                            return newAnswers;
+                          });
+                        }}
+                      />
+                      {question.answer02}
+                    </label>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <br />
+          <button
+            onClick={() => {
+              setPage((current) => {
+                return current + 1;
+              });
+            }}
+          >
+            이전 
+          </button>
+          <button
+            onClick={() => {
+              setPage((current) => {
+                return current + 1;
+              });
+            }}
+            disabled={isButtonDisabled}
+          >
+            다음 
+          </button>
         </div>
-    );
-}
+      );
+    };
+    
+    export default Test;
 
-export default Test;
+    
