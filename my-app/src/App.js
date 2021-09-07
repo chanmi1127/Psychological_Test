f69c101f14ffef66d16358d8b1bbddd7097e32e9
Chanmi <chanmi1127@gmail.com>
2021-09-07 23:47:31 +0900

    Feat: GA 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import { React, useEffect} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Test from './Test';
 import TestFinished from './TestFinished';
 import TestResult from './TestResult';
+import ReactGA from 'react-ga';
 
 
 function App() {
+    useEffect(() => {
+        ReactGA.initialize('UA-206689346-1');
+        ReactGA.pageview(window.location.pathname + window.location.search);
+      }, []) 
     return (
         <div>
             <Switch>
